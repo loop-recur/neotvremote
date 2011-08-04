3452cc548f54a11eb3e7e1a9db230ec4ce2e7774
Taylor Smith <tjsmith19@gmail.com>
2011-08-03 19:18:06 -0500

    settings index

--- Resources/views/settings/index.js
+++ Resources/views/settings/index.js
@@ -1,26 +1,24 @@
 Views.settings.index = function(win, settings) {	
 
-	var inputData = [
-		{title:'row 2', owner : {}, header:'Connect to a device'},
-		{title:'row 2', owner : {}},
-		{title:'row 3', owner : {}},
-		{title:'Add a new device', owner : {} , header:''},
-	];
+	var newRow = Ti.UI.createTableViewRow({
+		title:"Add a new device",
+		owner:{},
+		header:""
+	});
+	
+	var rows = map(createTableViewRow, settings);
+	
+	rows.push(newRow);
 	
 	var tableView = Titanium.UI.createTableView({
-		data:inputData,
+		data:rows,
 		style:Titanium.UI.iPhone.TableViewStyle.GROUPED
 	});
 		
 	function createTableViewRow(r) {
-		var row = Ti.UI.createTableViewRow({
-			height:80,
-			width:320
-		});
+		var row = Ti.UI.createTableViewRow();
 		
 		var title = Ti.UI.createLabel({
-			left:100,
-			top:17,
 			height:20,
 			width:190,
 			text:r.host
@@ -41,23 +39,10 @@ Views.settings.index = function(win, settings) {
 		return row;
 	}
 	
-	var rows = map(createTableViewRow, settings);
-
-	inputData.push(rows);
-	
 	tableView.addEventListener('click', function(e)
 	{
 		Views.settings.show(win, e.rowData.owner);
 	});
-	
-	// var tableview = Titanium.UI.createTableView({ 
-	// 	data:rows
-	//  });
-	// 
-	// tableview.addEventListener('click', function(e) {
-	// 	Views.settings.show(win, e.rowData.owner);
-	// });
-	// 
 
 	win.add(tableView);	
 };
