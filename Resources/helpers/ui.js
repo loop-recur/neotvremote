5d326d523df7305c64cfa48181d97d00ff2fc0ba
Taylor Smith <tjsmith19@gmail.com>
2011-08-11 21:11:08 -0600

    button updates, confirm on power down

--- Resources/helpers/ui.js
+++ Resources/helpers/ui.js
@@ -17,3 +17,20 @@ Helpers.ui.keyboard = function() {
 	
 	return keyboard_field;
 }
+
+Helpers.ui.confirm = function(title, callbacks) {
+	if (!callbacks.cancel) callbacks.cancel = function() {};
+	var alert = Titanium.UI.createAlertDialog({ 
+		title:title, 
+		message: "Are you sure?", 
+		buttonNames: ['Yes', 'Cancel'], 
+		cancel:1 
+	});
+	
+	alert.addEventListener('click', function(e) { 
+		if (e.cancel === e.index || e.cancel === true) {return;}
+		(e.index === 0) ? callbacks.yes() : callbacks.cancel();
+	});
+		
+	alert.show();
+};
