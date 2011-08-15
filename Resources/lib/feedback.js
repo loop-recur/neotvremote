696216c6f3894fe3a3c694423f4bc30156f51757
drboolean <brian@lateporter.com>
2011-08-15 09:20:06 -0700

    feedback partly fixed

--- Resources/lib/feedback.js
+++ Resources/lib/feedback.js
@@ -1,8 +1,14 @@
 var Feedback = function() {
+	var soundFun = Helpers.Application.isAndroid() ? androidSound : iOSSound;
 	
-	function buttonPress() {
-		// if(!Helpers.Application.isAndroid()) Titanium.Media.playClick();
+	function androidSound() {
+		Titanium.Media.createSound({url:url,preload:true,allowBackground:true});
+	}
+	
+	// shouldn't need this
+	function iOSSound() {
+		Titanium.Media.playClick();
 	}
 
-	return {buttonPress: buttonPress}
+	return {buttonPress: soundFun}
 }();
