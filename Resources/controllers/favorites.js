c4ee33d5c99fd7dd7f22319df22b8c3bc12d9a5f
drboolean <brian@lateporter.com>
2011-08-16 10:42:11 -0700

    resolved

--- Resources/controllers/favorites.js
+++ Resources/controllers/favorites.js
@@ -2,11 +2,7 @@ Controllers.favorites = function() {
 	var name = "favorites";
 	
 	function index(view, params) {
-<<<<<<< HEAD
-		App.db.find("favorites", {}, compose(view, _makeFavs));
-=======
 		App.db.find(name, {}, compose(view.partial(params), _makeFavs));
->>>>>>> 3eba643ec22b66e1aa7f7925e28fc58e7df056c7
 	}
 	
 	function edit(view, params) {
